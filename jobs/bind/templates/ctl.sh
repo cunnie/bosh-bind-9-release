6064540a501d8cebf173d0308a69bb754bbb4684
Brian Cunnie <brian.cunnie@gmail.com>
2015-03-11 11:02:14 -0700

    control script

---
+++ jobs/bind/templates/ctl.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+RUN_DIR=/var/vcap/sys/run/bind
+LOG_DIR=/var/vcap/sys/log/bind
+PIDFILE=${RUN_DIR}/pid
+
+case $1 in
+
+  start)
+    mkdir -p $RUN_DIR $LOG_DIR
+    chown -R vcap:vcap $RUN_DIR $LOG_DIR
+
+    echo $$ >> $PIDFILE
+
+    cd /var/vcap/packages/bind
+
+    exec /var/vcap/packages/bin/bin/named -u vcap
+
+    ;;
+
+  stop)
+
+    PID=$(cat $PIDFILE)
+    if [ -n $PID ]; then
+      SIGNAL=0
+      N=1
+      while kill -$SIGNAL $PID 2>/dev/null; do
+        if [ $N -eq 1 ]; then
+          echo "waiting for pid $PID to die"
+        fi
+        if [ $N -eq 11 ]; then
+          echo "giving up on pid $PID with kill -0; trying -9"
+          SIGNAL=9
+        fi
+        if [ $N -gt 20 ]; then
+          echo "giving up on pid $PID"
+          break
+        fi
+        n=$(($N+1))
+        sleep 1
+      done
+    fi
+
+    rm -f $PIDFILE
+
+    ;;
+
+  *)
+    echo "Usage: ctl {start|stop}" ;;
+
+esac
